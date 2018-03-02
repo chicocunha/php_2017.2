3696565c39329beeb2d7055ff44a3cc6bcd5f632
51115152017.2 <51115152017.2@51DE194262.senacrjedu.local>
2018-03-01 21:55:33 -0300

    primeiro comit

---
+++ hello_world.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <header>
+        <meta charset = "UTF-8">
+        <title>Hello World</title>
+    </header>
+    <body>
+        <h1>Minha primeira pagina em PHP com codigo basico "Ola mundo"</h1>
+        <code>
+    <!--para iniciarmos um bloco de codigo PHP usamos as tags abaixo-->
+    <?php
+        # para declaramos uma variavel e nacesssario usar o simbolo
+        $hello = "Ola mundo php";
+        
+        // comondo utilizado para imprimir(saida) na tela
+           echo($hello);
+           
+           /*
+           Detalhes importantes
+            em php sempre sera necessario iniciar uma variavel com $.$_C
+            Para que o servidor entenda os codigo php os arquivos devem ser salvos  com extensao php.
+
+           */
+           ?>
+           </code>
+        </body>
+    </html>
