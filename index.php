d7b0837bd7e3efca66d2cf3cfc5092bbb1de63c0
51115152017.2 <51115152017.2@51DE194262.senacrjedu.local>
2018-03-06 22:03:33 -0300

    primeiro comit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--autor; fran
+local; Senac niteroi
+Data; 6/3/2018
+finalidade;pagina principal do projeto
+
+-->
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        $var1=10;
+        $var2=20;
+        $var3=$var1+$var2;
+        
+        echo$var3;
+        ?>
+    </body>
+</html>
---
+++ tipo_dados.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+# Variavel armexenando um valor
+$texto = "Segunda aula de PHP";
+# variavel armazenando um valor real
+$inteiro = 02;
+#Variavel armazenando um valor real
+$real =45.15;
+# Vareavel armazenando um valor carctere
+$letra = 'L';
+# variavel armazenando um boleano
+$boleano = true;
+ var_dump($texto);
+ echo "<br>";
+ var_dump($inteiro);
+ echo "<br>";
+ var_dump($real);
+ echo "<br>";
+ var_dump($letra);
+ echo "<br>";
+ var_dump($boleano);
